fa2d6e68c20475184b4fa92152647acf16414642
Sergey Uchaev <dialoggg1994@gmail.com>
2023-02-24 14:17:14 +0300

    add connect for DB

---
+++ repositories/users.py
@@ -0,0 +1,15 @@
+from .base import BaseRepository
+
+class UsersRepository(BaseRepository):
+    async def get_all(limit: int = 10, offset: int = 0):
+        return
+
+    async def get_by_id(id: int):
+        return
+
+    async def create():
+        return
+
+    async def get_by_email(email: str):
+        return
+
